934f473a5d1b97d4b42ee3b2ee02432802dcfe74
thefirmt <thefirmt@gmail.com>
2019-08-06 19:46:28 -0700

    finished all

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+const fsPromise = Promise.promisifyAll(require('fs'));
 
 var items = {};
 
@@ -24,41 +26,20 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  return new Promise(function(resolve, reject) {
-    var result;
-    var list = fs.readdirSync(exports.dataDir)
-      result = _.map(list, (filename) => {
-        console.log(filename);
+  return fsPromise.readdirAsync(exports.dataDir)
+    .then((list) => {
+      return _.map(list, (filename) => {
         var number = filename.match(/[0-9]/gi).join("");
-        var data = fs.readFileSync(path.join(exports.dataDir, `${number}.txt`));
+        return fsPromise.readFileAsync(path.join(exports.dataDir, `${number}.txt`))
+          .then((data) => {
             data = data.toString('utf8');
-            console.log({id: number, text: data});
             return ({id: number, text: data});
-        });
-        console.log(result)
-        return result
+          });
       });
-
-  // return new Promise((resolve, reject)) {
-  //   fs.readdir(exports.dataDir, (err, list) => {
-  //     if (!list){
-  //       return list;
-  //     }
-  //     var result = _.map(list, (text) => {
-  //       var number = text.match(/[0-9]/gi).join("");
-  //       return {"id": number};
-  //     })
-  //     .then((idObj) => {
-
-  //     });
-  //     callback(err, result);
-  //   }
-  // });
-
-  // var data = _.map(items, (text, id) => {
-  //   return { id, text };
-  // });
-  // callback(null, data);
+    }).then((file) => {
+      return Promise.all(file);
+    })
+    .then((todo) => callback(null, todo));
 };
 
 exports.readOne = (id, callback) => {
@@ -87,12 +68,12 @@ exports.update = (id, text, callback) => {
     }
     fs.writeFile(location, text, (err) =>{
       if (err){
-        callback(err, null)
+        callback(err, null);
       }else{
         callback(err, {id, text});
       }
-    })
-  })
+    });
+  });
   // var item = items[id];
   // if (!item) {
   //   callback(new Error(`No item with id: ${id}`));
